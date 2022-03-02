24d36c1c358f797dfaf8c135501d4a3a8ecd5216
Shalah <zoungranaala@gmail.com>
2022-03-02 14:37:24 -0500

    Added few routes in the homeroute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,4 +7,29 @@ router.get('/', async (req, res) => {
   res.render('homepage');
 });
 
+// Access to one user account
+// Use the custom middleware before allowing the user to access the account/page
+
+router.get('/painting/:id', withAuth, async (req, res) => {
+  try {
+    const dbPaintingData = await Painting.findByPk(req.params.id);
+
+    const painting = dbPaintingData.get({ plain: true });
+
+    res.render('painting', { painting, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
 module.exports = router;
