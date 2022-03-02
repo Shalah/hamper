20e5a0852b92ea3559d0e6a44b1fedc74410eead
Shalah <zoungranaala@gmail.com>
2022-03-02 14:32:53 -0500

    Working on logging sessions. Added a bunch of files but nothing is linked and working yet

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const withAuth = require('../utils/auth')
+
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
   res.render('homepage');
--- server.js
+++ server.js
@@ -3,11 +3,28 @@ const session = require('express-session');
 const exphbs =  require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+//const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.use(require('./controllers/'));
 
 app.use(express.static('public'));
 //Setup routes to the Server
