067cab6fbde5fea9e2503a225d2c7087dbf64d62
Chase Young <chasekyoung@gmail.com>
2022-03-04 14:16:59 -0500

    began work on new order page

--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,7 @@ console.log('Test');
 document.getElementById('signOut').onclick = function () {
   location.href = '/';
 };
+
+document.getElementById('newOrder').onclick = function () {
+  location.href = '/item';
+};
