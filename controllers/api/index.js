2b768d0a543cde31bb64f77e39715df3c794036e
Bryan Orbe <bryan.orbe@gmail.com>
2022-03-04 11:10:20 -0500

    added more route code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const homeRoutes = require('./homeRoutes');
+const itemRoutes = require('./itemRoutes')
 
 router.use('/users', userRoutes);
 router.use('/home', homeRoutes);
+router.use('/item', itemRoutes);
 
 module.exports = router;
