98b01dd516a5e073ce75515c2eddd5e7f1d84ca9
Shalah <zoungranaala@gmail.com>
2022-03-07 22:46:12 -0500

    Adding two JS files to handle querying the data from user to the database

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -1,190 +1,92 @@
 const router = require('express').Router();
 const Items_tb = require('../../models/Items_tb');
 
-const withAuth = require('../../utils/auth');
-
-router.post('/item', async (req, res) => {
-  try {
-    const itemData = await Items_tb.create({
-      item_type: req.body.item_type,
-      item_tone: req.body.item_tone,
-      item_size: req.body.item_size,
-      item_brand: req.body.item_brand,
-      is_cotton: req.body.is_cotton,
-      is_leather: req.body.is_leather,
-      is_linen: req.body.is_linen,
-    });
-    res.status(200).json(itemData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-  console.log(itemData);
-});
-
-router.put('/:id', async (req, res) => {
-  try {
-    const item = await Items_tb.update(
-      {
-        item_type: req.body.item_type,
-        item_tone: req.body.item_tone,
-        item_size: req.body.item_size,
-        item_brand: req.body.item_brand,
-        is_cotton: req.body.is_cotton,
-        is_leather: req.body.is_leather,
-        is_linen: req.body.is.linen,
-      },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
-    );
-
-    res.status(200).json(item);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-
-
-
-// User Selection
-
-
-
-router.post('/:id', withAuth, async (req, res) => {
-  console.log('req.body',req.body)
-  try {
-    const newItem = await Items_tb.create({ ...req.body, user_id: req.session.user_id });
-    res.json(newItem);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// router.post('/:id', (req, res) => {
-  
-//   Items_tb.bulkCreate(
-   
-//     const itemtype = () => {
-//       let userChoice;
-  
-//       if(userChoice === 'shirt'){
-//         "INSERT INTO hamperdc_db (items_tb) VALUES ('shirt')"
-//       }
-//       else if (userChoice === 'Pants'){
-//         "INSERT INTO hamperdc_db (items_tb) VALUES ('Pants')"
-//       }
-//       else if (userChoice === 'Pants'){
-//         "INSERT INTO hamperdc_db (items_tb) VALUES ('Pants')"
-//       }
-//       else if (userChoice === 'Skirt'){
-//         "INSERT INTO hamperdc_db (items_tb) VALUES ('Skirt')"
-//       }
-//       else if (userChoice === 'Blazer'){
-//         "INSERT INTO hamperdc_db (items_tb) VALUES ('Blazer')"
-//       }
-//       else if (userChoice === 'Coat'){
-//         "INSERT INTO hamperdc_db (items_tb) VALUES ('Coat')"
-//       }
-//       else if (userChoice === 'Sweater'){
-//         "INSERT INTO hamperdc_db (items_tb) VALUES ('Sweater')"
-//       }
-//       else if (userChoice === 'Dress'){
-//         "INSERT INTO hamperdc_db (items_tb) VALUES ('Dress');"
-// //       }
-// //   );
-//     .then(() => {
-//       res.send('items_tb seeded!');
-//     })
-//     .catch((err) => {
-//       res.json(err);
+// router.post('/Items_tb', async (req, res) => {
+//   try {
+//     const itemData = await Item.create({
+//       item_type: req.body.item_type,
+//       item_tone: req.body.item_tone,
+//       item_size: req.body.item_size,
+//       item_brand: req.body.item_brand,
+//       is_cotton: req.body.is_cotton,
+//       is_leather: req.body.is_leather,
+//       is_linen: req.body.is_linen,
 //     });
+//     res.status(200).json(itemData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+//   console.log(itemData);
 // });
 
+// router.put('/:id', async (req, res) => {
+//   try {
+//     const item = await Item.update(
+//       {
+//         item_type: req.body.item_type,
+//         item_tone: req.body.item_tone,
+//         item_size: req.body.item_size,
+//         item_brand: req.body.item_brand,
+//         is_cotton: req.body.is_cotton,
+//         is_leather: req.body.is_leather,
+//         is_linen: req.body.is.linen,
+//       },
+//       {
+//         where: {
+//           id: req.params.id,
+//         },
+//       }
+//     );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const itemtone = () => {
-//   let userChoice;
-//   if(userChoice === 'Dark'){
-
+//     res.status(200).json(item);
+//   } catch (err) {
+//     res.status(500).json(err);
 //   }
-//   else if (userChoice === 'Light'){
+// });
 
-//   }
-//   else if (userChoice === 'White'){
-    
-//   }
-//   else{
-//     alert(``)
-//   }
-// }
-
-// router.get('/:id', (req, res) => {
-
-//   const itemtype = () => {
-//     let userChoice;
-
-//     if(userChoice === 'shirt'){
-//       var sql = "INSERT INTO hamperdc_db (items_tb) VALUES ('shirt')"
-//     }
-//     else if (userChoice === 'Pants'){
-
-//     }
-//     else if (userChoice === 'Blouse'){
+router.post('/add-item', async(req, res) => {
+  console.log(`Trying new route`, req.body);
+  try{
+    const userItem = await Items_tb.create({
+      ...req.body
+      });
+      res.status(200).json(userItem);
       
-//     }
-// //     else if (userChoice === 'Skirt'){
-      
-// //     }
-// //     else if (userChoice === 'Blazer'){
-      
-//     }
-//     else if (userChoice === 'Coat'){
-      
-//     }
-//     else if (userChoice === 'Sweater'){
-      
-//     }
-//     else if (userChoice === 'Dress'){
-      
-//     }
-//   }
-// })
+    }
+
+    // if(!userItem){
+    //   console.log(userItem, `L 57`)
+    //   res.status(400).json({message: `Cannot send data`})
+    //   return;
+    // }
+  //}
+  catch(err){
+    res.status(400).json({message: `Cannot send data`})
+  }
+});
 
 
+// How to update
 
+// Adding a delete 
+router.delete('/', async (req, res) => {
+  try {
+    const userItem = await Items_tb.destroy({
+      where: {
+        email: req.params.email,
+        //id: req.session.id,
+      },
+    });
 
+    if (!userItem) {
+      res.status(404).json({ message: 'No account found!' });
+      return;
+    }
 
-
-
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
+
--- public/js/add-item.js
+++ public/js/add-item.js
@@ -1,57 +1,83 @@
+//const res = require("express/lib/response");
+
 const addBtn = document.getElementById('addBtn');
 
-function newFormHandler(event) {
-  console.log( 'L 4')
+// 
+
+
+
+// function addCard() {
+//   let type = document.getElementById('item_type').value;
+//   let tone = document.getElementById('item_tone').value;
+//   let material = document.getElementById('item_material').value;
+//   let brand = document.getElementById('item_brand').value;
+//   let size = document.getElementById('item_size').value;
+
+//   console.log(type, tone, material, brand, size);
+// }
+
+
+
+
+addBtn.addEventListener('click', addCard);
+//document.querySelector('#addBtn').addEventListener('submit', addCard);
+
+
+
+
+const addCard = async function(event) {
+  // prevents the page for reloading everytime
+  console.log("Checking button L22")
   event.preventDefault();
 
-  const item_type = document.querySelector('#item_type').value;
-  const item_tone = document.querySelector('#item_tone').value;
-  const item_size = document.querySelector('#item_size').value;
-  const is_cotton = document.querySelector('#is_cotton:checked') ? true : false;
-  const is_leather = document.querySelector('#is_leather:checked') ? true : false;
-  const is_linen = document.querySelector('#is_linen:checked') ? true : false;
-  const item_brand = document.querySelector('#item_brand').value;
+  let typeEl = document.getElementById('item_type');
+  let toneEl = document.getElementById('item_tone');
+  let materialEl = document.getElementById('item_material');
+  let brandEl = document.getElementById('item_brand');
+  let sizeEl = document.getElementById('item_size');
+  
 
-  //How to put item_brand value into a variable??
-  const response = fetch(`./api/itemRoutes.js`, {
+  // This is to get the user data and send it to the database
+  const response = await fetch('/api/item/add-item', {
     method: 'POST',
     body: JSON.stringify({
-      item_type,
-      item_tone,
-      item_size,
-      item_brand,
-      is_cotton,
-      is_leather,
-      is_linen,
+      item_type: typeEl.value,
+      item_tone: toneEl.value,
+      item_material: materialEl.value,
+      item_brand: brandEl.value,
+      item_size: sizeEl.value,
+      
     }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
-    document.location.replace('/homepage');
+    console.log(response, 'L 46')
+    
+    alert(' Data sent'); // To be deleted
+    console.log('success')
   } else {
-    alert('Failed to add item');
+    alert('Failed to send data!!');
+    console.log('failed')
+
   }
-}
-
-<<<<<<< HEAD
-addBtn.addEventListener('click', newFormHandler);
-
-//function addCard() {}
+};
 
 
-=======
-addBtn.addEventListener('click', addCard);
 
-function addCard() {
-  let type = document.getElementById('item_type').value;
-  let tone = document.getElementById('item_tone').value;
-  let material = document.getElementById('item_material').value;
-  let brand = document.getElementById('item_brand').value;
-  let size = document.getElementById('item_size').value;
 
-  console.log(type, tone, material, brand, size);
-}
->>>>>>> main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
